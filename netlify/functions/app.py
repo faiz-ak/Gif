0baf65286b386a9d4e1e9ae9e1d9dda4b0c39635
faizan-blip <faizanrock753@gmail.com>
2026-02-23 21:21:37 +0530

    latest commit 5

--- netlify/functions/app.py
+++ netlify/functions/app.py
@@ -1,5 +1,9 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 from serverless_wsgi import handle
+
 
 def handler(event, context):
     return handle(app, event, context)
